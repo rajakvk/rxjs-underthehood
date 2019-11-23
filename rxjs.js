9452cb11c0f10c76c1a61680c79b01c3f56593dd
Venkataswamy, Kanakaraj (kv760r) <kv760r@att.com>
2019-11-23 22:12:01 +0530

    Generic API to cover most of the cases

--- rxjs.js
+++ rxjs.js
@@ -1,20 +1,20 @@
 // https://www.youtube.com/watch?v=uQ1zhJHclvs
 
-// three call backs when stream of data comes in
-const readable = getReadableStreamSomehow();
-
-function nextDataCallback(chunk) {
-    console.log(`Received ${chunk.length} byes of data`);
+// Generic API to cover most of the cases
+function nextDataCallback(data) {
+    //
 }
 
 function errorCallback(err) {
-    console.log(`Bad stuff happened: ${err}`);
+    //
 }
 
 function doneCallback() {
-    console.log(`There will be no more data`);
+    //
 }
 
-readable.on('data', nextDataCallback);
-readable.on('error', errorCallback);
-readable.on('end', doneCallback);
+giveMeSomeData(
+    nextDataCallback,
+    errorCallback,
+    doneCallback
+);
